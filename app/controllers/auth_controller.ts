ed99d7b193ebbc0b4d1b51e79040013c7b748830
thibaut009 <gabet.thibaut@gmail.com>
2025-04-09 15:05:36 +0200

    Add user auth

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,3 +1,39 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import { loginValidator, registerValidator } from '#validators/auth'
+import User from '#models/user'
 
-export default class AuthController {}
+export default class AuthController {
+  async login({ request, response }: HttpContext) {
+    try {
+      const { email, password } = await request.validateUsing(loginValidator)
+      const user = await User.verifyCredentials(email, password)
+      const token = await User.accessTokens.create(user)
+
+      return response.ok({ token: token, ...user.serialize() })
+    } catch {
+      return response.badRequest({ message: 'Invalid credentials' })
+    }
+  }
+
+  async register({ request, response }: HttpContext) {
+    try {
+      const payload = await request.validateUsing(registerValidator)
+      const user = await User.create(payload)
+      return response.created(user)
+    } catch {
+      return response.badRequest({ message: 'Registration failed' })
+    }
+  }
+
+  async logout({ auth, response }: HttpContext) {
+    try {
+      const token = auth.user?.currentAccessToken.identifier
+      if (!token) return response.badRequest({ message: 'Token not found' })
+
+      await User.accessTokens.delete(auth.getUserOrFail(), token)
+      return response.ok({ message: 'Logged out' })
+    } catch {
+      return response.badRequest({ message: 'Logout failed' })
+    }
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,22 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+const AuthController = () => import('#controllers/auth_controller')
+
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.delete('logout', [AuthController, 'logout']).use(middleware.auth())
+    router
+      .get('me', async ({ auth, response }) => {
+        try {
+          const user = auth.getUserOrFail()
+          return response.ok(user)
+        } catch (error) {
+          return response.unauthorized({ error: 'User not found' })
+        }
+      })
+      .use(middleware.auth())
+  })
+  .prefix('auth')
